e7910b9f44b747b32c373bdae9cd8e7258242e00
Артем Яппаров <japparov@surfstudio.co>
2019-10-02 23:17:29 +0300

    добавлено чтение данных из базы данных

--- app/src/main/java/ru/chronicker/rsubd/database/DBHelper.kt
+++ app/src/main/java/ru/chronicker/rsubd/database/DBHelper.kt
@@ -1,12 +1,17 @@
 package ru.chronicker.rsubd.database
 
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import ru.chronicker.rsubd.DBConstants.DB_NAME
 import ru.chronicker.rsubd.DBConstants.DB_VERSION
-import ru.chronicker.rsubd.database.utils.ScriptConstructor
+import ru.chronicker.rsubd.database.base.Entity
 import ru.chronicker.rsubd.database.models.Disease
+import ru.chronicker.rsubd.database.utils.QueryResponse
+import ru.chronicker.rsubd.database.utils.ScriptConstructor
+
+private val UNKNOWN_ERROR = "Unknown Error"
 
 class DBHelper(context: Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION) {
 
@@ -33,6 +38,36 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_V
                 .forEach { entityName ->
                     database.execSQL(ScriptConstructor.formDrop(entityName))
                 }
+        }
+    }
+
+    fun selectAll(
+        entity: Entity,
+        transformer: (Cursor) -> Entity,
+        response: QueryResponse<List<Entity>>
+    ) {
+        val sqLiteDatabase = readableDatabase
+
+        var cursor: Cursor? = null
+        val result = mutableListOf<Entity>()
+        try {
+            cursor = sqLiteDatabase.query(entity.name, null, null, null, null, null, null)
+
+            if (cursor.moveToFirst()) {
+                do {
+                    result.add(transformer.invoke(cursor))
+                } while (cursor.moveToNext())
+
+                response.onSuccess(result)
+            } else {
+                response.onError("There are no student in database")
+            }
+
+        } catch (e: Exception) {
+            response.onError(e.message ?: UNKNOWN_ERROR)
+        } finally {
+            sqLiteDatabase.close()
+            cursor?.close()
         }
     }
 }
