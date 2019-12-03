8147c5b88dcabe356dd5125b7d0570e99eaca98d
Артём Яппаров <japparov@surfstudio.ru>
2019-12-03 22:04:48 +0300

    начата реализация экрана авторизации

---
+++ app/src/main/java/ru/chronicker/rsubd/ui/screens/auth/AuthActivity.kt
@@ -0,0 +1,13 @@
+package ru.chronicker.rsubd.ui.screens.auth
+
+import ru.chronicker.rsubd.R
+import ru.chronicker.rsubd.ui.base.BaseActivity
+
+class AuthActivity : BaseActivity() {
+    override val layoutId: Int
+        get() = R.layout.activity_auth
+
+    override fun initViews() {
+    }
+
+}
