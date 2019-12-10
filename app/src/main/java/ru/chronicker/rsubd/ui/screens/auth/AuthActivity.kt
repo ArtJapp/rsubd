69a93db67ade62534c2e674e15a35af38eae39d7
Артём Яппаров <japparov@surfstudio.ru>
2019-12-10 11:37:19 +0300

    добавлен запрос авторизации

--- app/src/main/java/ru/chronicker/rsubd/ui/screens/auth/AuthActivity.kt
+++ app/src/main/java/ru/chronicker/rsubd/ui/screens/auth/AuthActivity.kt
@@ -1,13 +1,33 @@
 package ru.chronicker.rsubd.ui.screens.auth
 
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_auth.*
 import ru.chronicker.rsubd.R
+import ru.chronicker.rsubd.database.DBHelper
 import ru.chronicker.rsubd.ui.base.BaseActivity
 
 class AuthActivity : BaseActivity() {
     override val layoutId: Int
         get() = R.layout.activity_auth
 
+    private lateinit var dbHelper: DBHelper
+
     override fun initViews() {
+        dbHelper = DBHelper(this)
+        submit_auth_btn.setOnClickListener {
+            val password = password_et.text.toString()
+            val login = login_et.text.toString()
+            dbHelper.authenticate(
+                login,
+                password,
+                {
+                    Log.d("auth", "auth succeed for role $it")
+                },
+                {
+                    Log.d("auth", "holy shit")
+                }
+            )
+        }
     }
 
 }
